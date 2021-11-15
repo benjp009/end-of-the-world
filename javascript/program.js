2e3c414c72e0ed20321dbd8555c4ffb0b126612f
benjp009 <patin.benjamin@gmail.com>
2021-11-15 23:33:36 +0100

    js linters

--- javascript/program.js
+++ javascript/program.js
@@ -1,36 +1,36 @@
 const program = [
   {
-    icon: "/ressources/images/cheers.png",
-    title: "Party",
-    description: "The end is near, so no more escuses, life is a party, Enjoy while you can.",
+    icon: '/ressources/images/cheers.png',
+    title: 'Party',
+    description: 'The end is near, so no more escuses, life is a party, Enjoy while you can.',
   },
   {
-    icon: "/ressources/images/yes.png",
-    title: "Accept",
-    description: "afther that day, the world won't be. Just accept it. We have sessions for you to work on that.",
+    icon: '/ressources/images/yes.png',
+    title: 'Accept',
+    description: 'afther that day, the world won't be. Just accept it. We have sessions for you to work on that.',
   },
   {
-    icon: "/ressources/images/flame.png",
-    title: "Burn",
-    description: "Your belongings will no longer be, it's time to burn it all! We have gasoline and matches!",
+    icon: '/ressources/images/flame.png',
+    title: 'Burn',
+    description: 'Your belongings will no longer be, it's time to burn it all! We have gasoline and matches!',
   },
   {
-    icon: "/ressources/images/heart.png",
-    title: "Love",
-    description: "The last act you should do is love, love everyone and receive love from all people present.",
+    icon: '/ressources/images/heart.png',
+    title: 'Love',
+    description: 'The last act you should do is love, love everyone and receive love from all people present.',
   },
   {
-    icon: "/ressources/images/destroy.png",
-    title: "Destroy",
-    description: "You should destroy all things that you touch, better by you than the destroyer of worlds, no?",
+    icon: '/ressources/images/destroy.png',
+    title: 'Destroy',
+    description: 'You should destroy all things that you touch, better by you than the destroyer of worlds, no?',
   },
 ];
 
 for (let i = 0; i < program.length; i += 1) {
   document.getElementById('program').innerHTML += `
-  <div class="program-card">
-    <img src="${program[i].icon}" alt="" class="program-icon">
-    <h4 class="program-title">${program[i].title}</h4>
-    <p class="program-text">${program[i].description}</p>
-  </div>`
+  <div class='program-card'>
+    <img src='${program[i].icon}' alt='' class='program-icon'>
+    <h4 class='program-title'>${program[i].title}</h4>
+    <p class='program-text'>${program[i].description}</p>
+  </div>`;
 }

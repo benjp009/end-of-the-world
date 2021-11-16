61fce7e4629fbbf35f11aa2ac4548dcdd6798a22
benjp009 <patin.benjamin@gmail.com>
2021-11-16 15:09:37 +0100

    change path

--- javascript/program.js
+++ javascript/program.js
@@ -1,26 +1,26 @@
 const program = [
   {
-    icon: './ressources/images/cheers.png',
+    icon: 'ressources/images/cheers.png',
     title: 'Party',
     description: 'The end is near, so no more escuses, life is a party, Enjoy while you can.',
   },
   {
-    icon: './ressources/images/yes.png',
+    icon: 'ressources/images/yes.png',
     title: 'Accept',
     description: 'afther that day, the world will nott be. Just accept it. We have sessions for you to work on that.',
   },
   {
-    icon: './ressources/images/flame.png',
+    icon: 'ressources/images/flame.png',
     title: 'Burn',
     description: 'Your belongings will no longer be, it is time to burn it all! We have gasoline and matches!',
   },
   {
-    icon: './ressources/images/heart.png',
+    icon: 'ressources/images/heart.png',
     title: 'Love',
     description: 'The last act you should do is love, love everyone and receive love from all people present.',
   },
   {
-    icon: './ressources/images/destroy.png',
+    icon: 'ressources/images/destroy.png',
     title: 'Destroy',
     description: 'You should destroy all things that you touch, better by you than the destroyer of worlds, no?',
   },

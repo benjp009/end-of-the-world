cc080a75b613c610237dd7f00f27507d1e7713af
benjp009 <patin.benjamin@gmail.com>
2021-11-17 10:47:35 +0100

    fix linters on js

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,10 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 /* Open */
 function openNav() {
-  document.getElementById("overlay-screen").style.height = "100%";
+  document.getElementById('overlay-screen').style.height = '100%';
 }
 
 /* Close */
 function closeNav() {
-  document.getElementById("overlay-screen").style.height = "0%";
+  document.getElementById('overlay-screen').style.height = '0%';
 }
